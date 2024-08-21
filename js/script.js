2f2518c92ea06757932381ba2899310019f01c20
raul-bonet <161500597+raul-bonet@users.noreply.github.com>
2024-08-21 14:22:16 -0300

    Add files via upload
    Crie um vetor com as palavras ['maçã', 'banana', 'uva', 'melancia', 'pera'] e imprimaapenas as que possuem mais de 5 caracteres.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-aaaaa
+
+
+let frutas = [];
+
+function adicionarFruta() {
+  const inputFruta = document.getElementById('fruta');
+  const fruta = inputFruta.value.trim();
+  if (fruta !== '') {
+    frutas.push(fruta);
+    inputFruta.value = '';
+    atualizarTabelaFrutas();
+  }
+}
+
+function atualizarTabelaFrutas() {
+  const tabelaFrutas = document.getElementById('tabelaFrutas');
+  const tbody = tabelaFrutas.getElementsByTagName('tbody')[0];
+  tbody.innerHTML = ''; 
+  frutas.forEach((fruta, index) => {
+    const row = tbody.insertRow(index);
+    const cell = row.insertCell(0);
+    cell.textContent = fruta;
+  });
+} 
+
+function mostrarCinco() {
+  const tabelaCinco = document.getElementById('TabelaCinco');
+  const tbody = tabelaCinco.getElementsByTagName('tbody')[0];
+  tbody.innerHTML = '';
+  frutas.filter(fruta => fruta.length > 5).forEach((fruta, index) => {
+    const row = tabelaCinco.insertRow(index);
+    const cell = row.insertCell(0);
+    cell.textContent = fruta;
+  });
+}
+
+/*Não entendi direito o exercício, eu fiz da forma que entendi mas acho que entendi errado.*/
